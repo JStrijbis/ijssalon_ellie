f5b22d7735b425fb7d431fbe95e3b4aa76c472b1
jeroen strijbis <jeroenstrijbis@gmail.com>
2024-04-24 21:59:58 +0200

    aanpassing tot opdracht 6 les 10

--- helper.py
+++ helper.py
@@ -5,3 +5,22 @@ def decoreer(tekst=""):
     print (f"* {tekst} *")
     print(lengte * "*")
     print()
+
+def fooi_pp(bedrag,personen):
+    bedrag_pp = bedrag/personen
+    return f'De fooi per persoon bedraagt {bedrag_pp}'
+
+def onderstreep(tekst=""):
+    uit = []
+    regel ="=-" * len(tekst)
+    uit.append(tekst)
+    uit.append (regel)
+    return uit
+
+def som(dictonary):
+    totaal = (sum(dictonary.values()))
+    return totaal
+
+
+
+    
